504d47c43ed7107c344dd19658bb65ffc712fba2
Christopher Huston <git@chrishuston.net>
2016-10-29 03:57:49 +0000

    finance calc

--- lesson_2/finance_calc.rb
+++ lesson_2/finance_calc.rb
@@ -12,30 +12,31 @@ def prompt(message)
 end
 
 def financing_amount
-  $amount_financed = 0
-  while $amount_financed <= 0 do
+  loop do
     puts "\n"
     prompt("Please input the loan amount: ")
     $amount_financed = gets.chomp.to_f
+    break if $amount_financed > 0
   end
 end
+    
 
 def percentage_rate
-  $apr = -1
-  while $apr < 0 || $apr > 100 do
+  loop do
     puts "\n"
     prompt("Please input the annual percentage rate (example - 5% is 5) : ")
     $apr = gets.chomp.to_f
     $monthly_apr = (($apr.to_f / 100) / 12)
+    break if $apr >= 0 && $apr <= 100
   end
 end
-
+  
 def loan_term
-  $term = 0
-  while $term < 1 do
+  loop do
     puts "\n"
     prompt("Please input the loan term in months (example - 5 yrs is 60 months) : ")
     $term = gets.chomp.to_f
+    break if $term >=1
   end
 end
 
